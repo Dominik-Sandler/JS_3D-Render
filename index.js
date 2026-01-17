57e945fb6885602cf8e45fbc302eb1429ed470f0
Dominik-Sandler <dsandler.me@gmail.com>
2026-01-17 19:13:10 +0100

    Added a shapes.js class, Added a createDonut

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ ANGLE = 0
 
 var ctx = canvas.getContext("2d");
 
-function vec2(x,y) {
-    return{x,y}
-}
 function vec3(x,y,z) {
     return{x,y,z}
 }
@@ -104,7 +101,7 @@ function draw(poslist){
     for(var i = 0; i < poslist.length; i++){
         ctx.fillStyle = RENDER_COLOR;
         ctx.fillRect(poslist[i].x,poslist[i].y,LINE_THICKNESS,LINE_THICKNESS)
-    } 
+    }
 }
 function draw1(poslist, edgelist) {
     ctx.strokeStyle = RENDER_COLOR;
@@ -136,24 +133,6 @@ function frame(){
     draw1(render(cube),cubeE)
     requestAnimationFrame(frame);
 }
-function createCubeV(){
-    return [
-        vec3(-0.5, 0.5, 0.5),vec3(0.5, 0.5, 0.5),
-        vec3(0.5, -0.5, 0.5),vec3(-0.5, -0.5, 0.5),
-        vec3(-0.5, 0.5, -0.5),vec3(0.5, 0.5, -0.5),
-        vec3(0.5, -0.5, -0.5),vec3(-0.5, -0.5, -0.5)
-    ];
-}
-
-function createCubeE(){
-    return [
-        {a:0, b:1}, {a:1, b:2}, {a:2, b:3}, {a:3, b:0},
-        {a:4, b:5}, {a:5, b:6}, {a:6, b:7}, {a:7, b:4},
-        {a:0, b:4}, {a:1, b:5}, {a:2, b:6}, {a:3, b:7} 
-    ];
-}
-
 
 initialze(CANVAS_WIDTH,CANVAS_HEIGHT,BG_COLOR) 
-
 frame()
