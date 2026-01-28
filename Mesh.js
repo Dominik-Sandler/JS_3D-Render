cfaef63ccbcc568d6711464b49bd4f66eb2e6d7c
Dominik-Sandler <dsandler.me@gmail.com>
2026-01-28 21:49:58 +0100

    Updated Mesh class , to ensure only triangles exist

--- Mesh.js
+++ Mesh.js
@@ -1,8 +1,13 @@
 class Mesh {
-  constructor(vertices, faces) {
+  constructor(vertices, faces,material) {
     this.vertices = vertices;
-    this.faces = faces;
+    this.triangles = [];
+     for (let f of faces) {
+      if (f.length === 3) this.triangles.push(f);
+      if (f.length === 4) this.triangles.push(...quadToTris(f));
+    }
     this.position = vec3(0,0,0);
     this.rotation = vec3(0,0,0);
+    this.material = material;
   }
 }
